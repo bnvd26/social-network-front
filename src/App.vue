f2ca9eb5a0baff8f7b27075b85afc69b219f074f
Benjamin <benjaminad5@yahoo.com>
2020-10-18 17:13:47 +0200

    feat(router) WIP

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,12 @@
 <template>
-  <div class="container">
-    <h1>Social Network</h1>
-      <b-card
-        title="Card Title"
-        img-src="https://picsum.photos/600/300/?image=25"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-    >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/login">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-
-export default {
-  name: 'App',
-}
-</script>
 
 <style>
 #app {
@@ -36,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Form />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Form from '@/components/Login/Form.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    Form
+  }
+}
+</script>
