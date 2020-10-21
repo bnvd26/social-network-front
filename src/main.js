4e4bc673965972a13b53b9c9244a186aff936dba
Benjamin <benjaminad5@yahoo.com>
2020-10-21 19:12:51 +0200

    feat(store) init vueX

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import router from './router'
+import { store } from './store'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store.js
@@ -0,0 +1,93 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+	state: {
+		user : {
+			me: [],
+			friends: [],
+			isAuthenticated: false
+		},
+		posts : {
+			myPosts: [],
+			data: [],
+		},
+	},
+	mutations: {
+		SAVE_ME(state, me) {
+			state.user.me = me;
+		},
+		IS_AUTHENTICATED(state, isAuthenticated) {
+			state.user.isAuthenticated = isAuthenticated
+		},
+		SAVE_POSTS(state, payload) {
+			state.posts.data = payload;
+		},
+		SAVE_MY_POSTS(state, payload) {
+			state.posts.myPosts = payload;
+		},
+		SAVE_MY_FRIENDS(state, payload) {
+			state.user.friends = payload;
+		}
+	},
+	actions: {
+		me({commit}) {
+			axios
+			.get("http://localhost:81/api/profile", {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			})
+			.then((response) => {
+				commit('SAVE_ME', response.data);
+				commit('IS_AUTHENTICATED', true);
+			})
+			.catch(() => {
+				commit('IS_AUTHENTICATED', false);
+			});
+		},
+		deconnection({commit}) {
+			commit('IS_AUTHENTICATED', false);
+		},
+		posts({commit}) {
+			axios
+			.get("http://localhost:81/api/posts", {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			})
+			.then((response) => {
+				commit('SAVE_POSTS', response.data);
+			})
+		},
+		myPosts({commit}) {
+			axios
+			.get("http://localhost:81/api/my-posts", {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			})
+			.then((response) => {
+				commit('SAVE_MY_POSTS', response.data);
+			})
+		},
+		friends({commit}) {
+			axios
+			.get("http://localhost:81/api/friends", {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			})
+			.then((response) => {
+				commit('SAVE_MY_FRIENDS', response.data);
+			})
+		}
+	},
+})
