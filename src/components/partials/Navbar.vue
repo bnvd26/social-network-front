43091025b52bd1a392df88e23e5168e5eca6e411
Benjamin <benjaminad5@yahoo.com>
2020-11-05 19:30:19 +0100

    feat(navbar) conditionnal navbar

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -1,21 +1,40 @@
 <template>
 	<div class="hello">
+		<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">BA26</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarsExample05">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						
+						<router-link to="home" v-if="user.isAuthenticated" class="nav-link">Home</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link to="friendlist" v-if="user.isAuthenticated" class="nav-link">Liste d'amis</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link to="login" v-if="!user.isAuthenticated" class="nav-link">Se connecter</router-link>
+					</li>
+					<li class="nav-item dropdown">
+						<router-link to="me" v-if="user.isAuthenticated" class="nav-link">Mon profil</router-link>
+					</li>
+				</ul>
+				<b-nav-form @submit="onSubmit" v-if="user.isAuthenticated">
+					<b-form-input v-model="form.s" class="form-control" placeholder="Search"></b-form-input>
+					<b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+				</b-nav-form>
+				<p @click="logout" class="text-white" v-if="user.isAuthenticated">Déconnexion</p>
+			</div>
+		</nav>
 		
 		<b-navbar type="dark" variant="dark">
+		
 			<b-navbar-nav>
-				<router-link to="home" v-if="user.isAuthenticated">Home</router-link>
-				<router-link to="friendlist" v-if="user.isAuthenticated">Liste d'amis</router-link>
-				<router-link to="login" v-if="!user.isAuthenticated">Se connecter</router-link>
-			</b-navbar-nav>
-			<b-nav-form @submit="onSubmit" v-if="user.isAuthenticated">
-				<b-form-input v-model="form.s" class="mr-sm-2" placeholder="Search"></b-form-input>
-				<b-button variant="outline-success" class="my-2 my-sm-0" type="submit"
-					>Search</b-button
-				>
-			</b-nav-form>
-			<b-navbar-nav>
-				<router-link to="me" v-if="user.isAuthenticated">Mon profil</router-link>
-				<p @click="logout" class="text-white" v-if="user.isAuthenticated">Déconnexion</p>
+				
+				
 			</b-navbar-nav>
 		</b-navbar>
 	</div>
